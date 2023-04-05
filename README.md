45bb8a3218783db2e5c0962193c4ce0e6e524dfa
Cyber OS Lab <{ID}+{username}@users.noreply.github.com>
2023-04-05 14:53:28 -0700

    up

--- README.md
+++ README.md
@@ -1,59 +1,59 @@
-# GitHub Action for fetching the devcard from [daily.dev](https://api.daily.dev/get?r=omBratteng)
+# GitHub Action for fetching the badge from [daily.dev](https://api.daily.dev/get?r=omBratteng)
 
-[![GitHub](https://img.shields.io/github/license/dailydotdev/action-devcard)](LICENSE)
-[![GitHub Workflow Status (event)](https://img.shields.io/github/workflow/status/dailydotdev/action-devcard/continuous-integration?event=push&label=continuous-integration&logo=github)](https://github.com/dailydotdev/action-devcard/actions/workflows/continuous-integration.yml)
-[![GitHub release (latest by date)](https://img.shields.io/github/v/release/dailydotdev/action-devcard?logo=github)](https://github.com/dailydotdev/action-devcard/releases/latest)
+[![GitHub](https://img.shields.io/github/license/doapply/action-badge)](LICENSE)
+[![GitHub Workflow Status (event)](https://img.shields.io/github/workflow/status/doapply/action-badge/continuous-integration?event=push&label=continuous-integration&logo=github)](https://github.com/doapply/action-badge/actions/workflows/continuous-integration.yml)
+[![GitHub release (latest by date)](https://img.shields.io/github/v/release/doapply/action-badge?logo=github)](https://github.com/doapply/action-badge/releases/latest)
 
 ## Example usage
 
 ```yaml
 jobs:
-  devcard:
+  badge:
     runs-on: ubuntu-latest
     permissions:
       contents: write
     steps:
-      - name: devcard
-        uses: dailydotdev/action-devcard@2.1.0
+      - name: badge
+        uses: doapply/action-badge@2.1.0
         with:
-          devcard_id: ${{ secrets.DEVCARD_ID }}
+          badge_id: ${{ secrets.badge_ID }}
 ```
 
 ## Action options
 
 ### Required
 
-- `devcard_id`: this is the unique hash of the devcard, it can be found in the URL of the devcard.
-  - e.g. `https://api.daily.dev/devcards/0b156485612243bfa39092f30071e276.png` where the devcard_id is `0b156485612243bfa39092f30071e276`
-  - Can be found at [https://app.daily.dev/devcard](https://app.daily.dev/devcard)
+- `badge_id`: this is the unique hash of the badge, it can be found in the URL of the badge.
+  - e.g. `https://api.daily.dev/badges/0b156485612243bfa39092f30071e276.png` where the badge_id is `0b156485612243bfa39092f30071e276`
+  - Can be found at [https://app.daily.dev/badge](https://app.daily.dev/badge)
 
 ### Optional
 
-- `token`: GitHub Token used to commit the devcard
-- `commit_branch`: The branch to commit the devcard to. Defaults to the branch of the action.
-- `commit_message`: The commit message to use when committing the devcard. Defaults to `Update ${filename}`.
-  - You can use `${filename}` in the message to refer to the filename of the devcard.
-- `commit_filename`: The filename to commit the devcard to. Defaults to `devcard.svg`.
-  - If you want to save the devcard as a PNG, you can use `devcard.png` instead, or any other filename ending in `.png`.
-- `dryrun`: If set to `true`, the action will run as normal, but not actually commit the devcard
+- `token`: GitHub Token used to commit the badge
+- `commit_branch`: The branch to commit the badge to. Defaults to the branch of the action.
+- `commit_message`: The commit message to use when committing the badge. Defaults to `Update ${filename}`.
+  - You can use `${filename}` in the message to refer to the filename of the badge.
+- `commit_filename`: The filename to commit the badge to. Defaults to `badge.svg`.
+  - If you want to save the badge as a PNG, you can use `badge.png` instead, or any other filename ending in `.png`.
+- `dryrun`: If set to `true`, the action will run as normal, but not actually commit the badge
 
 ## Advanced usage
 
-This will save the devcard as PNG and commit to a separate branch named `devcard`.
+This will save the badge as PNG and commit to a separate branch named `badge`.
 
 ```yaml
 jobs:
-  devcard:
+  badge:
     runs-on: ubuntu-latest
     permissions:
       contents: write
     steps:
-      - name: devcard
-        uses: dailydotdev/action-devcard@2.1.0
+      - name: badge
+        uses: doapply/action-badge@2.1.0
         with:
-          devcard_id: ${{ secrets.DEVCARD_ID }}
-          commit_branch: devcard
-          commit_filename: devcard.png
+          badge_id: ${{ secrets.badge_ID }}
+          commit_branch: badge
+          commit_filename: badge.png
           commit_message: "chore: update ${filename}"
 ```
 
--- src/index.ts
+++ src/index.ts
@@ -36,155 +36,155 @@ const fetchImagesFromSVG = async (svg: string): Promise<Record<string, string>> 
 	return images
 }
 
-const devcardURL = (devcard_id: string): string =>
-	`https://api.daily.dev/devcards/${devcard_id}.svg?r=${new Date().valueOf()}&ref=action`
+const badgeURL = (badge_id: string): string =>
+	`https://api.daily.dev/badges/${badge_id}.svg?r=${new Date().valueOf()}&ref=action`
 
-const validateDevcardIdAsUUID = (devcard_id: string): boolean => {
+const validatebadgeIdAsUUID = (badge_id: string): boolean => {
 	// An UUIDv4 regex without hyphens
 	const uuid4Regex = /^([0-9A-F]{8})([0-9A-F]{4})(4[0-9A-F]{3})([89AB][0-9A-F]{3})([0-9A-F]{12})$/i
-	return validate(devcard_id.replace(uuid4Regex, '$1-$2-$3-$4-$5'))
+	return validate(badge_id.replace(uuid4Regex, '$1-$2-$3-$4-$5'))
 }
 
-;(async function () {
-	try {
-		let devCardContent = ''
-
-		const devcard_id = core.getInput('devcard_id')
-		const token = core.getInput('token')
-		const branch = core.getInput('commit_branch')
-		const message = core.getInput('commit_message')
-		const filename = core.getInput('commit_filename')
-		const dryrun = core.getBooleanInput('dryrun')
-
-		// throw an error if filename is empty
-		if (!filename || filename.length === 0) {
-			throw new Error('Filename is required')
-		}
-
-		if (!validateDevcardIdAsUUID(devcard_id)) {
-			throw new Error(`Invalid devcard_id: ${devcard_id}`)
-		}
-
-		console.log(`Dryrun`, dryrun)
-
-		// Fetch the latest devcard
+	; (async function () {
 		try {
-			const res = await fetch(devcardURL(devcard_id))
-			devCardContent = await res.text()
-			const images = await fetchImagesFromSVG(devCardContent)
+			let badgeContent = ''
 
-			for (const image in images) {
-				if (Object.prototype.hasOwnProperty.call(images, image)) {
-					devCardContent = devCardContent.replace(image, await convertImageToBase64(images[image]))
+			const badge_id = core.getInput('badge_id')
+			const token = core.getInput('token')
+			const branch = core.getInput('commit_branch')
+			const message = core.getInput('commit_message')
+			const filename = core.getInput('commit_filename')
+			const dryrun = core.getBooleanInput('dryrun')
+
+			// throw an error if filename is empty
+			if (!filename || filename.length === 0) {
+				throw new Error('Filename is required')
+			}
+
+			if (!validatebadgeIdAsUUID(badge_id)) {
+				throw new Error(`Invalid badge_id: ${badge_id}`)
+			}
+
+			console.log(`Dryrun`, dryrun)
+
+			// Fetch the latest badge
+			try {
+				const res = await fetch(badgeURL(badge_id))
+				badgeContent = await res.text()
+				const images = await fetchImagesFromSVG(badgeContent)
+
+				for (const image in images) {
+					if (Object.prototype.hasOwnProperty.call(images, image)) {
+						badgeContent = badgeContent.replace(image, await convertImageToBase64(images[image]))
+					}
 				}
+
+				await fs.mkdir(path.dirname(path.join(`/tmp`, filename)), { recursive: true })
+				await fs.writeFile(path.join(`/tmp`, filename), badgeContent)
+
+				if (filename.endsWith('.png')) {
+					await sharp(path.join(`/tmp`, filename))
+						.png({
+							quality: 100,
+						})
+						.toFile(path.join(`/tmp`, `_${filename}`))
+
+					await fs.rename(path.join(`/tmp`, `_${filename}`), path.join(`/tmp`, filename))
+					console.log('Converted badge to PNG', 'ok')
+				}
+
+				console.log(`Saved to ${path.join(`/tmp`, filename)}`, 'ok')
+			} catch (error) {
+				console.debug(error)
 			}
 
-			await fs.mkdir(path.dirname(path.join(`/tmp`, filename)), { recursive: true })
-			await fs.writeFile(path.join(`/tmp`, filename), devCardContent)
+			const committer = {
+				commit: true,
+				message: message.replace(/[$][{]filename[}]/g, filename),
+				branch: branch || github.context.ref.replace(/^refs[/]heads[/]/, ''),
+				sha: undefined,
+			}
 
-			if (filename.endsWith('.png')) {
-				await sharp(path.join(`/tmp`, filename))
-					.png({
-						quality: 100,
+			const octokit = github.getOctokit(token)
+			console.log('Committer REST API', 'ok')
+			try {
+				console.log('Committer account', (await octokit.rest.users.getAuthenticated()).data.login)
+			} catch {
+				console.log('Committer account', '(github-actions)')
+			}
+
+			console.log('Using branch', committer.branch)
+
+			//Create head branch if needed
+			try {
+				await octokit.rest.git.getRef({ ...github.context.repo, ref: `heads/${committer.branch}` })
+				console.log('Committer head branch status', 'ok')
+			} catch (error) {
+				if (/not found/i.test(`${error}`)) {
+					const {
+						data: { object },
+					} = await octokit.rest.git.getRef({
+						...github.context.repo,
+						ref: github.context.ref.replace(/^refs[/]/, ''),
 					})
-					.toFile(path.join(`/tmp`, `_${filename}`))
 
-				await fs.rename(path.join(`/tmp`, `_${filename}`), path.join(`/tmp`, filename))
-				console.log('Converted devcard to PNG', 'ok')
+					console.log('Committer branch current sha', object.sha)
+					await octokit.rest.git.createRef({
+						...github.context.repo,
+						ref: `refs/heads/${committer.branch}`,
+						sha: object.sha,
+					})
+					console.log('Committer head branch status', '(created)')
+				} else throw error
 			}
 
-			console.log(`Saved to ${path.join(`/tmp`, filename)}`, 'ok')
-		} catch (error) {
-			console.debug(error)
-		}
-
-		const committer = {
-			commit: true,
-			message: message.replace(/[$][{]filename[}]/g, filename),
-			branch: branch || github.context.ref.replace(/^refs[/]heads[/]/, ''),
-			sha: undefined,
-		}
-
-		const octokit = github.getOctokit(token)
-		console.log('Committer REST API', 'ok')
-		try {
-			console.log('Committer account', (await octokit.rest.users.getAuthenticated()).data.login)
-		} catch {
-			console.log('Committer account', '(github-actions)')
-		}
-
-		console.log('Using branch', committer.branch)
-
-		//Create head branch if needed
-		try {
-			await octokit.rest.git.getRef({ ...github.context.repo, ref: `heads/${committer.branch}` })
-			console.log('Committer head branch status', 'ok')
-		} catch (error) {
-			if (/not found/i.test(`${error}`)) {
+			// Retrieve previous SHA of badge file to be able to update content through the API
+			try {
 				const {
-					data: { object },
-				} = await octokit.rest.git.getRef({
-					...github.context.repo,
-					ref: github.context.ref.replace(/^refs[/]/, ''),
-				})
-
-				console.log('Committer branch current sha', object.sha)
-				await octokit.rest.git.createRef({
-					...github.context.repo,
-					ref: `refs/heads/${committer.branch}`,
-					sha: object.sha,
-				})
-				console.log('Committer head branch status', '(created)')
-			} else throw error
-		}
-
-		// Retrieve previous SHA of devcard file to be able to update content through the API
-		try {
-			const {
-				repository: {
-					object: { oid },
-				},
-			} = await octokit.graphql<GraphQlQueryResponseData>(
-				`
+					repository: {
+						object: { oid },
+					},
+				} = await octokit.graphql<GraphQlQueryResponseData>(
+					`
 				query Sha {
 					repository(owner: "${github.context.repo.owner}", name: "${github.context.repo.repo}") {
 						object(expression: "${committer.branch}:${filename}") { ... on Blob { oid } }
 					}
 				}
 			`,
-				{ headers: { authorization: `token ${token}` } },
-			)
-			committer.sha = oid
+					{ headers: { authorization: `token ${token}` } },
+				)
+				committer.sha = oid
+			} catch (error) {
+				console.debug(error)
+			}
+			console.log('Previous render sha', committer.sha ?? '(none)')
+
+			// Compare previous to current badge
+			const git = sgit()
+			const sha = await git.hashObject(path.join(`/tmp`, filename))
+			console.log('Current badge sha', sha)
+			if (committer.sha === sha) {
+				console.log(`Commit to branch ${committer.branch}`, '(no changes)')
+				committer.commit = false
+			}
+
+			if (committer.commit && !dryrun) {
+				const fileContent = await fs.readFile(path.join(`/tmp`, filename))
+				await octokit.rest.repos.createOrUpdateFileContents({
+					...github.context.repo,
+					path: filename,
+					message: committer.message,
+					content: fileContent.toString('base64'),
+					branch: committer.branch,
+					...(committer.sha ? { sha: committer.sha } : {}),
+				})
+			}
 		} catch (error) {
-			console.debug(error)
+			if (error instanceof Error) {
+				core.setFailed(error.message)
+				console.debug(error)
+				process.exit(1)
+			}
 		}
-		console.log('Previous render sha', committer.sha ?? '(none)')
-
-		// Compare previous to current devcard
-		const git = sgit()
-		const sha = await git.hashObject(path.join(`/tmp`, filename))
-		console.log('Current devcard sha', sha)
-		if (committer.sha === sha) {
-			console.log(`Commit to branch ${committer.branch}`, '(no changes)')
-			committer.commit = false
-		}
-
-		if (committer.commit && !dryrun) {
-			const fileContent = await fs.readFile(path.join(`/tmp`, filename))
-			await octokit.rest.repos.createOrUpdateFileContents({
-				...github.context.repo,
-				path: filename,
-				message: committer.message,
-				content: fileContent.toString('base64'),
-				branch: committer.branch,
-				...(committer.sha ? { sha: committer.sha } : {}),
-			})
-		}
-	} catch (error) {
-		if (error instanceof Error) {
-			core.setFailed(error.message)
-			console.debug(error)
-			process.exit(1)
-		}
-	}
-})()
+	})()
